3f07acec506a488f5dc256e9c694a201e2954f27
Andy Joseph <neon_4@hotmail.com>
2024-07-29 20:55:31 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.css'
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).root.render(
   <React.StrictMode>
      <Router>
       <App />
